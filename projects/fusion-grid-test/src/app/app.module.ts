28ca2a8f918ba1d895933214955ead649dec9c30
sibythomas <siby.t@celestialsys.com>
2021-09-24 14:18:37 +0530

    Package name change

--- projects/fusion-grid-test/src/app/app.module.ts
+++ projects/fusion-grid-test/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-import { FusionGridModule } from 'fusion-grid';
+import { FusionGridModule } from 'angular-fusiongrid';
 
 import FusionGrid from '@fusioncharts/fusiongrid';
 
